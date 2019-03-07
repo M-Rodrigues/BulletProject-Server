527f9cd7c145237a3fb48dd20bb29644728f2bd8
Matheus <matheus.rodrigues.ime@gmail.com>
2019-03-07 08:54:11 -0300

    lista de usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -7,7 +7,7 @@ const router = new Router()
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-    const result = await db.query('SELECT * FROM teste', [])
+    const result = await db.query('SELECT * FROM usuarios', [])
     console.log(result)
     res.send(result);
 })
