d26355497e4878e2e00e5b9d5d7d29c49a8ca3d2
Matheus Rodrigues <matheus.rodrigues.ime@gmail.com>
2019-05-10 19:07:02 -0300

    crud usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -12,15 +12,40 @@ router.get('/', async (req, res, next) => {
     res.send(result);
 })
 
+router.get('/:id', async (req, res, next) => {
+    const result = await db.query('select * from usuarios where cod_usuario = $1', [parseInt(req.params.id)])
+    console.log(result)
+    res.send(result[0]);
+})
+
 router.post('/', async (req, res, next) => {
     try {
         const hashPsw = await bcrypt.hash(req.body.password, cred.saltRounds); // Criptografa senha
+        const hashAns = await bcrypt.hash(req.body.user.resposta, cred.saltRounds) // Criptografa resposta
 
         let user = req.body.user;                           // Cria novo usuario no banco
         const result = await db.query(
-            `insert into usuarios (nome, dia_nasc, mes_nasc, ano_nasc, email, senha)
-            values ($1, $2, $3, $4, $5, $6)`
-            ,[user.nome, user.dia_nasc, user.mes_nasc, user.ano_nasc, user.email, hashPsw]);
+            `insert into usuarios (nome, dia_nasc, mes_nasc, ano_nasc, email, resposta, senha)
+            values ($1, $2, $3, $4, $5, $6, $7)`
+            ,[user.nome, user.dia_nasc, user.mes_nasc, user.ano_nasc, user.email, hashAns, hashPsw]);
+
+        console.log(result);
+
+        if (result.erro) throw result.erro;
+
+        res.send({message: 'sucesso'});
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        console.log(req.params);
+        const result = await db.query(
+            `delete from usuarios where cod_usuario = $1`
+            ,[parseInt(req.params.id)]);
 
         console.log(result);
 
