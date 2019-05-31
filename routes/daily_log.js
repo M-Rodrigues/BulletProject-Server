b8ddace845cc63a910a6675508b7afdbed3e8a5e
Matheus Rodrigues <matheus.rodrigues.ime@gmail.com>
2019-05-31 14:24:38 -0300

    erro de portugues em "staus"

--- routes/daily_log.js
+++ routes/daily_log.js
@@ -141,7 +141,7 @@ router.delete('/:id', auth.authenticate, async (req, res, next) => {
 
         if (result.erro) throw result.erro;
 
-        res.send({staus: 0, message: 'sucesso'});
+        res.send({status: 0, message: 'sucesso'});
     } catch (err) {
         console.log(err);
         res.send({status: 1, erro:err });
